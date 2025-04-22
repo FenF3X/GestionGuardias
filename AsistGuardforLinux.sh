474efaa4d3a4321e3e069bd33fa150b65be244f2
Tu Nom <you@example.es>
2025-04-22 11:29:33 +0200

    fin

---
+++ AsistGuardforLinux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ruta o URL que deseas abrir
+URL="http://localhost/GestionGuardias/PROYECTO/REST/rest_cliente/login.php"
+
+# Abrir la URL en el navegador predeterminado
+xdg-open $URL
+
