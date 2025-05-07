43a3494c5a15da8628679e1a224b64d98ee20c1f
FenFex <FenFex@>
2025-05-07 10:06:58 +0200

    chat.css comentado y analizado (incluye eliminacion de botonNav.js por desuso)

--- PROYECTO/REST/rest_cliente/src/botonNav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.querySelector('.navbar-toggler');
-    const menuContent = document.getElementById('navbarContent');
-
-    toggleButton.addEventListener('click', function () {
-      menuContent.classList.toggle('show');
-    });
-  });
